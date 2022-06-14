8eee6cad2a497097f8a742cc72a563926c583832
Miguel <Miguel>
2022-06-14 18:14:43 -0300

    primer comittttttttttttt

---
+++ funciones.js
@@ -0,0 +1,15 @@
+
+
+(() => {
+        let patas = 5;
+
+    console.log("hola pianolas");
+ 
+
+})();
+
+
+
+//operadores ternarios
+let patas = 4;
+patas === 4 ? console.log(`el perro tiene ${patas} patas`) : alert("NOOO");
